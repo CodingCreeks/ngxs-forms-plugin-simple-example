5e314c533cc9361bdb2232f36b4935a196cb9a9c
CodingCreeks <halappa.manjunatha@gmail.com>
2019-06-14 10:32:26 -0400

    2 files modified

--- src/app/sample/sample.component.ts
+++ src/app/sample/sample.component.ts
@@ -1,15 +1,30 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { Select } from '@ngxs/store';
+import { FormState } from '../app.state';
+import { Observable } from 'rxjs';
+import { FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-sample',
   templateUrl: './sample.component.html',
   styleUrls: ['./sample.component.css']
 })
-export class SampleComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
+export class SampleComponent {
+  @Select(FormState) form$: Observable<any>;
+  form: FormGroup;
+  roles: Array<string> = [
+    'Guest',
+    'Admin',
+    'Owner',
+    'Operator'
+  ];
+  constructor(private fb: FormBuilder) {
+    this.form = this.fb.group({
+      'firstName': [''],
+      'lastName': [''],
+      'role': [''],
+      'notes': ['']
+    });
   }
 
 }
